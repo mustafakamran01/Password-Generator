a7782a1c6424d4ed8d2fc48d3ba0c4c6dc7f5ac3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-13 23:56:39 +0530

    first commit

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [password, setPassword] = useState("")
+
+  return (
+    <div className="max-w-xl mx-auto p-6 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
+      <h3 className="text-lg font-semibold mb-4">Input & Controls</h3>
+      <div className="flex items-center gap-2">
+        
+        <input
+        type="text"
+        value
+        placeholder='password'
+        readOnly
+        className="flex-1 px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-300 bg-gray-700"
+        />
+
+        <button
+        className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition hover:text-red-500 hover:bg-white"
+        title="Copy to clipboard"
+        >Copy</button>
+
+      </div>
+
+      <div className="mt-4 text-gray-800">
+
+        <label className="block text-sm font-medium mb-1 cursor-pointer">Length: {length}</label>
+        <input
+        type="range"
+        min={1}
+        max={100}
+        className="w-full"/>
+
+      </div>
+
+      <div className="mt-4 flex items-center gap-6">
+
+        <label className="inline-flex items-center gap-2 text-gray-800">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Character</span>
+        </label>
+
+        <label className="inline-flex items-center gap-2 text-gray-800">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Number</span>
+        </label>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
