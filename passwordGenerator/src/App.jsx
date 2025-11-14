e6f7ee51ef6ace0a0613f9599f7573113604dd0c
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-15 00:42:12 +0530

    Updating UI

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -36,14 +36,14 @@ function App() {
   // Copying text(password) to clipboard
   const copyToClipboard = () => {
     passwordRef.current?.select()
-    passwordRef.current?.setSelectionRange(0, 10)
+    passwordRef.current?.setSelectionRange(0, 100)
     window.navigator.clipboard.writeText(password)
   }
 
   return (
-    <div className="max-w-xl mx-auto p-6 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
-      <h3 className="text-lg font-semibold mb-4">Input & Controls</h3>
-      <div className="flex items-center gap-2">
+     <div className="max-w-xl mx-auto p-9 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
+      <h3 className="text-2xl font-semibold mb-4 text-black">Password Generator</h3>
+      <div className="flex items-center gap-1">
         
         <input
         type="text"
@@ -51,20 +51,20 @@ function App() {
         placeholder='password'
         readOnly
         ref={passwordRef}
-        className="flex-1 px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-300 bg-gray-700"
+        className="flex-1 px-10 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-800 bg-amber-100 text-black"
         />
 
         <button
         onClick={copyToClipboard}
-        className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition hover:text-red-500 hover:bg-white"
+        className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition"
         title="Copy to clipboard"
         >Copy</button>
-
+        
       </div>
 
-      <div className="mt-4 text-gray-800">
+      <div className="mt-4 text-rose-500">
 
-        <label className="block text-sm font-medium mb-1 cursor-pointer">Length: {length}</label>
+        <label className="block text-sm font-medium mb-1 cursor-pointer">LENGTH : {length}</label>
         <input
         type="range"
         min={1}
@@ -77,7 +77,7 @@ function App() {
 
       <div className="mt-4 flex items-center gap-6">
 
-        <label className="inline-flex items-center gap-2 text-gray-800">
+        <label className="inline-flex items-center gap-2 text-stone-900">
         <input
         type="checkbox"
         onChange={ () => setCharAllowed( (prev) => !prev)}
@@ -85,7 +85,7 @@ function App() {
         <span>Character</span>
         </label>
 
-        <label className="inline-flex items-center gap-2 text-gray-800">
+        <label className="inline-flex items-center gap-2 text-stone-900">
         <input
         type="checkbox"
         onChange={ () => setNumberAllowed( (prev) => !prev)}
